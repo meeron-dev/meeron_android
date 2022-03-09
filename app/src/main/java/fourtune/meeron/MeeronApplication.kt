ce2b4a90e9b58124d5ab865d87912e9294be4945
zozero94 <zozero94@gmail.com>
2022-03-10 03:15:17 +0900

    키해시를 출력해보자...;;

--- app/src/main/java/fourtune/meeron/MeeronApplication.kt
+++ app/src/main/java/fourtune/meeron/MeeronApplication.kt
@@ -1,13 +1,17 @@
 package fourtune.meeron
 
 import android.app.Application
+import android.widget.Toast
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MeeronApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        val keyHash = Utility.getKeyHash(this)
         KakaoSdk.init(this, BuildConfig.APP_KEY)
+        Toast.makeText(this, keyHash, Toast.LENGTH_SHORT).show()
     }
 }

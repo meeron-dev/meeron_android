1667398006a5cb28d82eeb6dfb105c72d84c5efc
zozero94 <zozero94@gmail.com>
2022-03-20 00:26:09 +0900

    로그인화면 디자인

--- presentation/src/main/java/fourtune/meeron/presentation/ui/login/LoginScreen.kt
+++ presentation/src/main/java/fourtune/meeron/presentation/ui/login/LoginScreen.kt
@@ -3,19 +3,16 @@ package fourtune.meeron.presentation.ui.login
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import fourtune.meeron.presentation.R
@@ -35,21 +32,34 @@ fun LoginScreen(viewModel: LoginViewModel = hiltViewModel(), isLoginSuccess: () 
             if (isLoginSuccess) isLoginSuccess()
         }
     }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 23.dp, vertical = 90.dp),
     ) {
+
         Image(
-            modifier = Modifier
-                .size(300.dp)
-                .clickable { viewModel.launchKakaoLogin(context) },
-            painter = painterResource(id = R.drawable.button_kakao_login),
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(id = R.drawable.ic_meeron_symbol),
             contentDescription = null
         )
-        Button(onClick = { viewModel.logout() }) {
-            Text(text = "로그아웃", color = colorResource(id = R.color.black))
-        }
+
+        Image(
+            modifier = Modifier
+                .clickable { viewModel.launchKakaoLogin(context) }
+                .align(Alignment.BottomCenter),
+            painter = painterResource(id = R.drawable.ic_kakao_login),
+            contentDescription = null
+        )
+
     }
 
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    LoginScreen {
+
+    }
 }
